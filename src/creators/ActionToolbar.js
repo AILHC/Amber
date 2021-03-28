6463bcd4c1fb153b642e94675de331e1d6c3cebd
bsgbryan <bsgbryan@gmail.com>
2021-03-28 02:07:39 -0500

    Added types to ActionToolbar buttons

--- src/creators/ActionToolbar.js
+++ src/creators/ActionToolbar.js
@@ -16,12 +16,12 @@ const styles = {
 const Component = ({ reset, create, createDisabled }) =>
   <div className="btn-toolbar d-flex justify-content-center" role="toolbar" aria-label="Actions available for this Direcitonal Light">
     <div className="btn-group me-2" role="group" aria-label="Undo">
-      <button className="btn btn-primary" onClick={e => { reset(); e.preventDefault( ) }}>
+      <button type="reset" className="btn btn-primary" onClick={e => { reset(); e.preventDefault( ) }}>
         <FontAwesomeIcon icon={faTrashUndoAlt} size="3x" style={styles} />
       </button>
     </div>
     <div className="btn-group" role="group" aria-label="Save">
-      <button className="btn btn-primary" onClick={e => { create(); e.preventDefault() }} disabled={createDisabled}>
+      <button type="submit" className="btn btn-primary" onClick={e => { create(); e.preventDefault() }} disabled={createDisabled}>
         <FontAwesomeIcon icon={faSave} size="3x" style={styles} />
       </button>
     </div>
